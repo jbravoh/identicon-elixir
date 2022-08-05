6b17d86ca65efaa1465d3e68c322a6fa37afdeea
jbravoh <jbravoharriott@gmail.com>
2022-08-05 15:04:57 +0100

    create the pixel map

--- lib/image.ex
+++ lib/image.ex
@@ -1,5 +1,5 @@
 defmodule Identicon.Image do
   # store list of numbers as the hex property
-  # define struct with the properties 'hex', 'colour' and 'grid' with both default values as 'nil'
-  defstruct hex: nil, colour: nil, grid: nil
+  # define struct with the properties 'hex', 'colour', 'grid'and 'pixe_map' with both default values as 'nil'
+  defstruct hex: nil, colour: nil, grid: nil, pixel_map: nil
 end
